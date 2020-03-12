b431406393ef51f3163f86ec9b6b91109a303a7e
hothdung <33243839+hothdung@users.noreply.github.com>
2020-03-13 03:44:49 +0900

    integrating emojis into the circles, tuning the sizes, and adding the labels next to the bars

--- teacherui/src/AtmosphereChart.js
+++ teacherui/src/AtmosphereChart.js
@@ -12,6 +12,7 @@ class AtmosphereChart extends Component {
             yAxisAttribute: "nameEmotion",
             xAxisAttribute: "percentage",
             xAxisAttribute2: "total",
+            img: "image",
             width: 400,
             height: 350
         }
@@ -20,6 +21,10 @@ class AtmosphereChart extends Component {
     }
 
     drawChart() {
+
+        var config = {
+            "emoji_size": 10
+        }
 
         var colors = ['#FF0000', '#5588ff', '#993299', '#A3A319', '#19D219'];
 
@@ -34,11 +39,13 @@ class AtmosphereChart extends Component {
             .domain([0, 100])
             .range([0, width]);
 
+        // y axis 
         let y = d3.scaleBand()
             .range([0, height])
             .domain(this.state.data.map((d) =>
                 d[this.state.yAxisAttribute]))
             .padding(0.1)
+
 
 
         let svg = d3.select(this.refs.canvas)
@@ -56,6 +63,10 @@ class AtmosphereChart extends Component {
             .style("font-size", "15px")
             .style("font-weight", "bold")
             .text("Class atmosphere")
+
+        svg.append('g')
+            .attr('class', 'yaxis')
+            .call(d3.axisLeft(y))
 
         // creating overlapping bar chart to create filling a static bar with certain percentage
         svg.selectAll("totalBar")
@@ -85,23 +96,48 @@ class AtmosphereChart extends Component {
             .attr('width', (d) => x(d[this.state.xAxisAttribute]))
             .attr('opacity', 1)
 
+        svg.append('defs')
+            .selectAll('pattern')
+            .data(this.state.data)
+            .enter()
+            .append('pattern')
+            .attr('id', function (d) {
+                return d.nameEmotion;
+            })
+            .attr('width', 70)
+            .attr('height', 90)
+            .append('image')
+            .attr("xlink:href", function (d) {
+                return d.image;
+            })
+            .attr('width', 40)
+            .attr('height', 40)
+            .attr("x", 0)
+            .attr("y", 0)
 
-        // attaching circle to percentag bars
-        svg.selectAll('circle')
+
+        // attaching circle to percentage bars
+        var circle = svg.selectAll('circle')
             .data(this.state.data)
             .enter()
             .append('circle')
             .attr('opacity', '0')
             .attr('cx', x(0.5))
             .attr('cy', (d) => y(d[this.state.yAxisAttribute]) + 15)
-            .attr('r', 22)
+            .attr('r', 20)
+            .style("stroke", function (d, i) {
+                return color(i);
+            })
+            .style("stroke-width", 4.5)
+            .attr('fill', function (d) {
+                return "url(#" + d.nameEmotion + ")";
+            })
             .transition().delay((d, i) => {
                 return i * 2000;
             })
             .duration(2000)
             .attr("cx", (d) => x(d[this.state.xAxisAttribute]))
             .attr('opacity', 1)
-
     }
 
 
