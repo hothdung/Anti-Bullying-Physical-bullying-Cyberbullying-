cf3bbaf813484a2c7cc66a5ff345f30e71d07912
hothdung <ho.thdung@gmail.com>
2020-03-19 14:27:30 +0900

    adding table frame Material UI

--- teacherui/src/Area.js
+++ teacherui/src/Area.js
@@ -11,6 +11,7 @@ import AtmosphereChart from './AtmosphereChart';
 import EmotionData from './data/classAtmosphere.json';
 import ReportingChart from './ReportingChart';
 import ReportingData from './data/reportMethods.json';
+import MethodsTable from './MethodsTable';
 
 class Area extends Component {
 
@@ -25,6 +26,7 @@ class Area extends Component {
                     <Col lg='7'>
                         <WarningFrequency warningVal={FrequencyData} />
                         <ReportingChart reportingMethods={ReportingData} />
+                        <MethodsTable />
                     </Col>
                     <Col lg='3'>
                         <AtmosphereChart emotionsVal={EmotionData} />
