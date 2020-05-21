dceeef9d9cd653c7573b3d5707ae85d1f45fcebc
hothdung <ho.thdung@gmail.com>
2020-05-21 19:02:38 +0900

    test

--- server/audioTranscribe.py
+++ server/audioTranscribe.py
@@ -1,17 +1,20 @@
+import re
 import speech_recognition as sr
 import sys
 import json
 
 
 def read_in():
-    lines = sys.stdin.readLines()
-    return json.loads(lines[0])
+    lines = sys.stdin.readlines()
+    # return json.loads(lines[0])
+    return lines
 
 
 def convertToString():
     print("Here in python method!")
-    lines = read_in()
-    audio = lines
+    # lines = read_in()
+    # audio = lines
+    audio = "test2.wav"
     r = sr.Recognizer()
     text = ""
 
@@ -21,12 +24,13 @@ def convertToString():
     try:
         text = r.recognize_google(audio, language='ko-kr')
         print(text)
-        # text_file = open("test6.txt", "w")
-        # n = text_file.write(text)
-        # text_file.close()
+        text_file = open("test6.txt", "w")
+        n = text_file.write(text)
+        text_file.close()
     except Exception as e:
         print(e)
-    return text
+
+    print(text.encode('utf8'))
 
 
 def main():
